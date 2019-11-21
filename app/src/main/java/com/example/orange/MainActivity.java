c542ffdc8b6560bc5b55e27cb1b9701183491b8a
GOKU <gannarala@gmail.com>
2019-11-21 11:26:29 +0100

    Formation-Android-Day-1

--- app/src/main/java/com/example/orange/MainActivity.java
+++ app/src/main/java/com/example/orange/MainActivity.java
@@ -1,22 +1,44 @@
 package com.example.orange;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.analytics.FirebaseAnalytics;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    EditText email, password;
+
     private FirebaseAnalytics mFirebaseAnalytics;
+    private FirebaseAuth mAuth;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        email = findViewById(R.id.mail_auth);
+        password = findViewById(R.id.password_auth);
+
+
+        //Firebase instances
         mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
+        mAuth = FirebaseAuth.getInstance();
 
         findViewById(R.id.sign_uph).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -24,5 +46,65 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(new Intent(MainActivity.this, Sign_up.class));
             }
         });
+
+
+
+        findViewById(R.id.sign_in).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                test();
+            }
+        });
     }
+
+
+    public void test()
+    {
+        mAuth.createUserWithEmailAndPassword(email.getText().toString(), password.getText().toString())
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task)
+                    {
+                        if (task.isSuccessful())
+                        {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.e("MainActivity", "createUserWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+                        }
+                        else
+                        {
+                            // If sign in fails, display a message to the user.
+                            Log.e("MainActivity", "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(MainActivity.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
+                            updateUI(null);
+                        }
+
+                        // ...
+                    }
+                });
+
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        updateUI(currentUser);
+    }
+
+    public void updateUI(FirebaseUser user)
+    {
+        if (user != null)
+        {
+            startActivity(new Intent(MainActivity.this, Activity_home.class));
+        }
+
+    }
+
+
+
+
 }
